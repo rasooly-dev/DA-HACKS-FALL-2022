a855ff7e70b477967677584277a8b7dfac276bf9
alessandro-pianetta <alessandro.pianetta@gmail.com>
2022-10-08 15:00:51 -0700

    Changes header img and makes width variable

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,16 @@ import Layout from "../components/layout";
 import Seo from "../components/seo";
 import SearchBar from "../components/SearchBar";
 
-import heroPic from "../images/Open Doodles - Reading.png";
+import heroPic from "../images/studying.png";
 
 // import UserCard from "../components/UserCard/UserCard";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <section className="py-5 text-center container">
-      <div>
-        <img src={heroPic} />
+    <section className="py-5 text-center justify-content-center container">
+      <div className="col-md-6 mx-auto">
+        <img className="img-fluid" src={heroPic} />
       </div>
       <div className="row py-lg-5">
         <div className="col-lg-6 col-md-8 mx-auto">
